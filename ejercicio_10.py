c3e8a6c901cc23406840ba8801b48ff39d52e133
leoalayonpinto <leonardoj.alayonp@gmail.com>
2026-08-27 23:34:05 +0200

    Delete ejercicio_10.py

--- ejercicio_10.py
+++
@@ -1,9 +0,0 @@
-# comprension de lista
-# Method 1 
-list=[]
-for i in range(1,11):
-    list.append(i) # Append method add a value in an empty list or filled list https://docs.python.org/3/library/stdtypes.html#list.append
-print(list)
-#Method 2 
-L=[i for i in range(1,11)] #list comprension
-print(L)
